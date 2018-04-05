52cb1e1530f539d252c54a4d879376903ec08003
rajtrilo <rtrilokekar@college.harvard.edu>
2018-04-05 15:07:26 -0400

    everything but canon v2

--- music.ml
+++ music.ml
@@ -183,9 +183,9 @@ let rec list_to_stream (lst : obj list) : event NLS.stream =
       match nlst with
       |[] -> list_to_stream lst
       |h::tl -> (match h with
-        |Note(p,t,i) -> fun () ->
+        |Note(p,t,i) ->
           Cons(Tone(0.,p,i), fun()->Cons(Stop(t,p), list_to_stream_rec tl))
-        |Rest(f) -> fun()->
+        |Rest(f) ->
           Cons(Tone(0.,(A,0),0),fun()->Cons(Stop(f,(A,0)),
             list_to_stream_rec tl))
     )
