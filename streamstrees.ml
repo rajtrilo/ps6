30ebb9c1dd6e6eac0d6bf8e7cf1f78a23d43cc96
rajtrilo <rtrilokekar@college.harvard.edu>
2018-04-06 16:09:09 -0400

    final commit

--- streamstrees.ml
+++ streamstrees.ml
@@ -212,4 +212,4 @@ on average, not in total).  We care about your responses and will use
 them to help guide us in creating future assignments.
 ......................................................................*)
 
-let minutes_spent_on_part () : int = failwith "not provided" ;;
+let minutes_spent_on_part () : int = 0 ;;
